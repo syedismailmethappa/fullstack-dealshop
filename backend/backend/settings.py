816f3dd8253af777116a282afb2e50c1ac1ae60e
syedismailmethappa <syedmethappa0987@gmail.com>
2025-11-08 00:24:28 +0530

    l

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,6 +62,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
