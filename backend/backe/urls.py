a3bd2b9b41d85d5b9af0cb34835b681bd37142f6
syedismailmethappa <syedmethappa0987@gmail.com>
2025-11-08 21:52:04 +0530

    v

--- backend/backe/urls.py
+++ backend/backe/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include, re_path
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter 
 from .views import ProductViewSet
-from django.views.generic import TemplateView
 
 
 router = DefaultRouter()
@@ -9,7 +8,6 @@ router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('', include(router.urls)),
-      re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
